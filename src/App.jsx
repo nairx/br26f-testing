adfe8af5fba4543da6b3e07219131b493b4193c3
nairx <nairxprv@gmail.com>
2026-06-04 15:56:19 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,61 @@
-
 // npm create vite@latest .
 // npm install -D vitest @testing-library/react @testing-library/jest-dom jsdom
 // npm install -D @testing-library/user-event
 
-// vitest → Test runner (like Jest but faster) 
-// @testing-library/react → Test React components 
-// @testing-library/jest-dom → Extra matchers (toBeInTheDocument) 
+// vitest → Test runner (like Jest but faster)
+// @testing-library/react → Test React components
+// @testing-library/jest-dom → Extra matchers (toBeInTheDocument)
 // jsdom → Simulates browser
 
 // setupTests.js inside src folder
 // import '@testing-library/jest-dom’;
 
-import React, { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 export default function App() {
-  const [message,setMessage] = useState("")
+  const [user, setUser] = useState({});
+  const [message, setMessage] = useState();
+  const handleSubmit = () => {
+    if (user.email === "john@gmail.com" && user.password === "1234") {
+      setMessage("Welcome");
+    } else {
+      setMessage("Access Denied");
+    }
+  };
   return (
     <div>
-      <h1>Hello World</h1>
+      <h3>Login Form</h3>
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Email"
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+        />
+      </p>
+      <button onClick={handleSubmit}>Submit</button>
       <p>{message}</p>
-      <p><button onClick={()=>setMessage("Broadridge")}>Update</button></p>
     </div>
-  )
+  );
 }
 
+// import React, { useState } from 'react'
+// export default function App() {
+//   const [message,setMessage] = useState("")
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <p>{message}</p>
+//       <p><button onClick={()=>setMessage("Broadridge")}>Update</button></p>
+//     </div>
+//   )
+// }
 
 // import React from 'react'
 // export default function App() {
@@ -30,4 +63,3 @@ export default function App() {
 //     <div>HELLO world</div>
 //   )
 // }
-
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import App from "./App";
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import App from "./App";
 
-test("renders hello world",()=>{
-    render(<App/>)
-    const txt = screen.getByText(/hello world/i)
-    expect(txt).toBeInTheDocument()
-})
+// test("renders hello world",()=>{
+//     render(<App/>)
+//     const txt = screen.getByText(/hello world/i)
+//     expect(txt).toBeInTheDocument()
+// })
 
-test("message update", async () => {
-  render(<App />);
-  const btn = screen.getByText("Update");
-  await userEvent.click(btn)
-  const txt = screen.getByText(/Broadridge/i);
-  expect(txt).toBeInTheDocument();
-});
+// test("message update", async () => {
+//   render(<App />);
+//   const btn = screen.getByText("Update");
+//   await userEvent.click(btn)
+//   const txt = screen.getByText(/Broadridge/i);
+//   expect(txt).toBeInTheDocument();
+// });
 
 // import React from "react";
 // import { render,screen } from "@testing-library/react";
